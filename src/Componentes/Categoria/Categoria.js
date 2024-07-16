f256fa5fd86fd829b0fda982f91cedb77c297c12
emmanuelcampos5 <emmanuelcampos2111@gmail.com>
2024-07-16 14:05:20 -0600

    diseño de cartas

--- src/Componentes/Categoria/Categoria.js
+++ src/Componentes/Categoria/Categoria.js
@@ -7,8 +7,8 @@ const Categoria = (props) =>{
     const {colaboradores} = props
 
     return <section className="categoria">
-        <h3 style={{background: colorPrimario}}>{titulo}</h3>
-        <div className="colaboradores">
+        <h3 className="text-center" style={{background: colorPrimario}}>{titulo}</h3>
+        <div className="colaboradores d-flex justify-content-center gap-5 row">
             {
                 colaboradores.map((colaborador) => (<Colaborador key={colaborador.id} colorPrimario={colorPrimario} datos = {colaborador} eliminarVideo={props.eliminarVideo}></Colaborador>))
             }
